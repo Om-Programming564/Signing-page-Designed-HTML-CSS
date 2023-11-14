21bc99282076048b2697c9d332079613b7560345
Om-Programming564 <150767967+Om-Programming564@users.noreply.github.com>
2023-11-14 10:14:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const submit = document.querySelector(".submit");
+const name = document.querySelector(".Name");
+const email = document.querySelector(".Email");
+const pass = document.querySelector(".Pass");
+
+function inputData() {
+  let name = name.value,
+  email = email.valueg,
+  pass = pass.value;
+}
+submit.addEventListener("click", () => {
+    console.log(inputData());
+    localStorage.value();
+    if(value.fill){
+        path = "D:\\Laptop Data\\DCIM\\OM Collecction\\Websites Responsive and feturable\\My Mind Sign in page\\ChatBox.html"
+    }
+});
